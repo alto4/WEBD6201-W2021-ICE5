0a38b612ab4bbe70793ea7ba233563d5f2d609cc
Tom Tsiliopoulos <tsiliopoulos@hotmail.com>
2021-02-10 09:15:39 -0500

    added working edit page

--- Scripts/app.js
+++ Scripts/app.js
@@ -101,7 +101,9 @@
 
             if(contact.serialize())
             {
-              localStorage.setItem((localStorage.length + 1).toString(), contact.serialize());
+              let key = contact.FullName.substring(0, 1) + Date.now();
+
+              localStorage.setItem(key, contact.serialize());
             }
           }
         });
@@ -116,7 +118,8 @@
         let data = "";
 
         let keys = Object.keys(localStorage);
-
+         
+        let index = 1;
 
         for (const key of keys) 
         {
@@ -126,20 +129,21 @@
           contact.deserialize(contactData);
 
           data += `<tr>
-          <th scope="row">${key}</th>
+          <th scope="row">${index}</th>
           <td>${contact.FullName}</td>
           <td>${contact.ContactNumber}</td>
           <td>${contact.EmailAddress}</td>
           <td class="text-center"><button value="${key}" class="btn btn-primary btn-sm edit"><i class="fas fa-edit fa-sm"></i> Edit</button></td>
           <td class="text-center"><button value="${key}" class="btn btn-danger btn-sm delete"><i class="fas fa-trash-alt fa-sm"></i> Delete</button></td>
-        </tr>`;
+          </tr>`;
+
+          index++;
         }
 
         contactList.innerHTML = data;
 
-        //TODO - need to create an edit page
         $("button.edit").on("click", function(){
-          console.log($(this).val());
+          location.href = "edit.html#" + $(this).val();
          });
 
          $("button.delete").on("click", function(){
@@ -152,7 +156,51 @@
       }
     }
 
-     
+    function displayEdit()
+    {
+      let key = location.hash.substring(1);
+
+      let contact = new core.Contact();
+
+      // check to ensure that the key is not empty
+      if(key != "")
+      {
+        // get contact info from localStorage
+        contact.deserialize(localStorage.getItem(key));
+
+        // display contact information in the form
+        $("#fullName").val(contact.FullName);
+        $("#contactNumber").val(contact.ContactNumber);
+        $("#emailAddress").val(contact.EmailAddress);
+      }
+
+      $("#editButton").on("click", function() 
+      {
+        // check to see if key is empty
+        if(key == "")
+        {
+          // create a new key
+          key = contact.FullName.substring(0, 1) + Date.now();
+        }
+
+        // copy contact info from form to contact object
+        contact.FullName = $("#fullName").val();
+        contact.ContactNumber = $("#contactNumber").val();
+        contact.EmailAddress = $("#emailAddress").val();
+
+        // add the contact info to localStorage
+        localStorage.setItem(key, contact.serialize());
+
+        // return to the contact list
+        location.href = "contact-list.html";
+      });
+
+      $("#cancelButton").on("click", function()
+      {
+        // return to the contact list
+        location.href = "contact-list.html";
+      });
+    }
 
     function Start()
     {
@@ -177,7 +225,10 @@
             break;
           case "Contact-List":
             displayContactList();
-          break;
+            break;
+          case "Edit":
+            displayEdit();
+            break;
         }
         
     }
