8ad1798658eeccb086460cec23860f742a199d73
Scott Alton <scottaltonmusic@gmail.com>
2021-02-18 18:01:40 -0500

    Fixed User class typos.

--- Scripts/user.js
+++ Scripts/user.js
@@ -47,14 +47,14 @@
     // constructor
     /**
      * Creates a new User instance
-     * @param {string} [DisplayName=""] 
+     * @param {string} [displayName=""] 
      * @param {string} [emailAddress=""]
      * @param {string} [username=""] 
      * @param {string} [password=""] 
      */
-    constructor(DisplayName = "", emailAddress = "", username = "", password = "") 
+    constructor(displayName = "", emailAddress = "", username = "", password = "") 
     {
-      this.DisplayName = fullName;
+      this.DisplayName = displayName;
       this.EmailAddress = emailAddress;
       this.Username = username;
       this.Password = password;
